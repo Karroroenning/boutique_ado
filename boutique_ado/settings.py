98900656ba64ba8452ada1cb97cc91a41a50dc60
karolina Rönning <Karroroenning@gmail.com>
2023-09-03 08:50:58 +0000

    added toasts

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -78,6 +78,8 @@ TEMPLATES = [
     },
 ]
 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 SITE_ID = 1
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'

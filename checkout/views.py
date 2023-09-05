ca4841c38fe8ffdafaae93b4882c050d0cfda056
karolina Rönning <Karroroenning@gmail.com>
2023-09-05 08:30:38 +0000

    added stripe element

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NmspWBl8IIfQs0BvDvruTI6vk2SN2VSoQ63cxslFo9K3LrDJakrQqj5mS4DR5BqabzbQE2XGe7aC8mlKBFDPg6j00zLzpYdMr',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
